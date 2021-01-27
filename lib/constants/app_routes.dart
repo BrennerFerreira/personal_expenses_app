4d0354807168d13fe760d31e7425c7a44f8df218
BrennerFerreira <brennercsferreira@gmail.com>
2021-01-27 17:23:22 -0300

    Corrected app label.

--- lib/constants/app_routes.dart
+++
@@ -1,6 +0,0 @@
-class AppRoutes {
-  // ignore: constant_identifier_names
-  static const String PAGE_VIEW = '/';
-  // ignore: constant_identifier_names
-  static const String TRANSACTION_FORM = '/transaction-form';
-}
